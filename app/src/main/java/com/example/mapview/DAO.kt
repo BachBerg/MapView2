5c77d602a0853c36d5d7049ba4e1608fbc645f5a
BachBerg <andreas-bach-nielsen@hotmail.com>
2022-06-08 23:57:49 +0200

    Diverse rettelser.

--- app/src/main/java/com/example/mapview/DAO.kt
+++ app/src/main/java/com/example/mapview/DAO.kt
@@ -2,27 +2,29 @@ package com.example.mapview
 
 import android.content.Context
 import android.util.Log
-import androidx.compose.ui.platform.LocalContext
 import com.google.firebase.firestore.FirebaseFirestore
+
+/* data access objekt
+* This class manages communication with firebase */
 
 class DAO {
     var db: FirebaseFirestore = FirebaseFirestore.getInstance()
     private val TAG = "DatabaseObject"
 
-    fun readFromDatabase(Document : String,context: Context){
-    val docRef = db.collection("Beacons").document(Document)
-    docRef.get()
-        .addOnSuccessListener() { document ->
-            if (document != null) {
-                Log.d(TAG, "Data: ${document.data}")
-                val notish = Notification(context, "BeaconNumber","${document.data}")
-                notish.sendNotification()
-            } else {
-                Log.d(TAG, "Dokumentet findes ikke ")
+    fun readFromDatabase(Document: String, context: Context) {
+        val docRef = db.collection("Beacons").document(Document)
+        docRef.get()
+            .addOnSuccessListener() { document ->
+                if (document != null) {
+                    Log.d(TAG, "Data: ${document.data}")
+                    val notish = Notification(context, "BeaconNumber", "${document.data}")
+                    notish.sendNotification()
+                } else {
+                    Log.d(TAG, "Dokumentet findes ikke ")
+                }
             }
-        }
-        .addOnFailureListener { exception ->
-            Log.d(TAG, "Dokumentet fejlede pga. ", exception)
-        }
-}
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "Dokumentet fejlede pga. ", exception)
+            }
+    }
 }
