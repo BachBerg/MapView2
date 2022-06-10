1724e893cfe8037a578dea4f774fb1f73ab769ed
Naveed <supershah2300@gmail.com>
2022-06-10 12:59:31 +0200

    kun nr ved notif

--- app/src/main/java/com/example/mapview/DAO.kt
+++ app/src/main/java/com/example/mapview/DAO.kt
@@ -14,13 +14,14 @@ class DAO {
     fun readFromDatabase(Document: String, context: Context) {
         val docRef = db.collection("Beacons").document(Document)
         docRef.get()
-            .addOnSuccessListener() { document ->
+            .addOnSuccessListener() { document ->9
                 if (document != null) {
                     Log.d(TAG, "Data: ${document.data}")
 
                     val data = "${document.data}".split(",")
+                    val datanr = data[1].split("=")
 
-                    val notish = Notification(context, "Du er nu ved position: ", data[1])
+                    val notish = Notification(context, "Du er nu ved position: ", datanr[1])
                     notish.sendNotification()
                 } else {
                     Log.d(TAG, "Dokumentet findes ikke ")
